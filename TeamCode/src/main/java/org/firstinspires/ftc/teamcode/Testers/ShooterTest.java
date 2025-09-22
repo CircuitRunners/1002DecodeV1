43e739ff91680ae75585473c1a0a39e2b367c3cf
Dillan Vuong <dillan.v88@gmail.com>
2025-09-22 16:28:56 -0400

    blah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/ShooterTest.java
@@ -6,12 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
-
 @TeleOp
 public class ShooterTest extends OpMode {
     public DcMotorEx shooter;
     public GamepadEx player1;
-    final double LAUNCHER_TARGET_VELOCITY = 1633;
+    double LAUNCHER_TARGET_VELOCITY = 1633; //velocity in ticks per second; 28 ticks per revolution
+    final double STOP_SPEED = 0.0;
 
     @Override
     public void init() {
@@ -25,9 +25,28 @@ public class ShooterTest extends OpMode {
 
         player1.readButtons();
 
-        if (player1.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+        if (player1.wasJustPressed(GamepadKeys.Button.CIRCLE)) {
             shooter.setVelocity(LAUNCHER_TARGET_VELOCITY);
         }
+        if (player1.wasJustPressed(GamepadKeys.Button.SQUARE)) {
+            shooter.setVelocity((STOP_SPEED));
+        }
+        if (player1.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+            LAUNCHER_TARGET_VELOCITY += 100;
+        }
+        if (player1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+            LAUNCHER_TARGET_VELOCITY -= 100;
+        }
+        if (player1.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
+            LAUNCHER_TARGET_VELOCITY += 10;
+        }
+        if (player1.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) {
+            LAUNCHER_TARGET_VELOCITY -= 10;
+        }
+
+        telemetry.addData("Target Speed", LAUNCHER_TARGET_VELOCITY);
+        telemetry.addData("Motor Speed", shooter.getVelocity());
+        telemetry.update();
     }
 }
 
