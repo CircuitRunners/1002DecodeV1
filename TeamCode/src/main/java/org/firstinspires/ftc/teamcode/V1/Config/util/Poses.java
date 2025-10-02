3ee5d8f53f7ae6cd96db4f61abce739c44fda7c2
Connor Reddington <99690237+CWRedd@users.noreply.github.com>
2025-10-02 15:28:26 -0400

    hypothetical poses for goal side auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V1/Config/util/Poses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V1/Config/util/Poses.java
@@ -19,9 +19,9 @@ public class Poses {
             this.bluePose = bluePose;
         }
 
-        // Mirror convenience: Blue is computed automatically
-        public static AlliancePose mirror(Pose redPose) {
-            return new AlliancePose(redPose, redPose.mirror());
+        // Mirror convenience: Red is computed automatically
+        public static AlliancePose mirror(Pose bluePose) {
+            return new AlliancePose(bluePose.mirror(), bluePose);
         }
 
         public Pose get(Alliance alliance) {
@@ -62,7 +62,7 @@ public class Poses {
     // Example poses
     // =======================
 
-    // ️Mirrored point (Blue auto-computed)
+    // ️Mirrored point (Red auto-computed)
     public static final AlliancePose startPose = AlliancePose.mirror(
             new Pose(7.3285, 65.83, 0)
     );
@@ -73,11 +73,35 @@ public class Poses {
             new Pose(-40, 60, Math.toRadians(10))  // Blue custom
     );
 
-    //  Another mirrored one
-    public static final AlliancePose intakePose = AlliancePose.mirror(
-            new Pose(20, 36, Math.toRadians(90))
+    // ===============
+    // Goal Side Poses (Blue given, red auto-computed
+    //================
+    public static final AlliancePose startPoint = AlliancePose.mirror(
+            new Pose(64, 136, 0)
     );
 
+    //Used multiple times cause shoot from same spot
+    public static final AlliancePose shootPosition = AlliancePose.mirror(
+            new Pose(48, 95.4, Math.toRadians(135))
+    );
+
+    public static final AlliancePose lineupLine1 = AlliancePose.mirror(
+            new Pose(41, 84, Math.toRadians(180))
+    );
+    public static final AlliancePose pickupLine1 = AlliancePose.mirror(
+            new Pose(24, 84, Math.toRadians(180))
+    );
+    public static final AlliancePose lineupLine2 = AlliancePose.mirror(
+            new Pose(41, 60, Math.toRadians(180))
+    );
+    public static final AlliancePose pickupLine2 = AlliancePose.mirror(
+            new Pose(24, 60, Math.toRadians(180))
+    );
+    public static final AlliancePose lineupAtGate = AlliancePose.mirror(
+            new Pose(19, 70, 0)
+    );
+
+    //TELEOP STUFF
     public static final AlliancePose teleopDefaultPose = AlliancePose.mirror(
             new Pose(72, 72, 0) // change to your preferred TeleOp "spawn" location
     );
