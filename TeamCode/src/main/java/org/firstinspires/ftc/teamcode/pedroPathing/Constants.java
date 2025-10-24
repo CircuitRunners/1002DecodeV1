f952af7fe9798cbc69be6874d6c9f2c72d8248d5
Connor Reddington <99690237+CWRedd@users.noreply.github.com>
2025-10-24 13:15:14 -0400

    new pedro tuner - also we need to remeasure the offsets of odo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,13 +16,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(5.9)
-            .forwardZeroPowerAcceleration(-50.36)
-            .lateralZeroPowerAcceleration(-65.82)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.04,0,0,0))
-            .headingPIDFCoefficients(new PIDFCoefficients(0.85, 0, 0.035, 0.03))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.0001,0.6,0.01))
-            .centripetalScaling(0.0005);
+//            .mass(5.9)
+//            .forwardZeroPowerAcceleration(-50.36)
+//            .lateralZeroPowerAcceleration(-65.82)
+//            .translationalPIDFCoefficients(new PIDFCoefficients(0.04,0,0,0))
+//            .headingPIDFCoefficients(new PIDFCoefficients(0.85, 0, 0.035, 0.03))
+//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.0001,0.6,0.01))
+            //.centripetalScaling(0.0005)
+            ;
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.8, 1);
 
@@ -46,15 +47,15 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(90.62)
-            .yVelocity(73.51)
+//            .xVelocity(90.62)
+//            .yVelocity(73.51)
 
             .useBrakeModeInTeleOp(true);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(1)
             .strafePodX(2.3)
-            .distanceUnit(DistanceUnit.INCH)
+            .distanceUnit(DistanceUnit.MM)
             .hardwareMapName("odo")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
